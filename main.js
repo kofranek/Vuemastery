a3b622d48099376c7612d35decc5bb27e2f099f1
Jiří Kofránek <kofranek@gmail.com>
2021-04-24 00:16:23 +0200

    lesson  4

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp
    data () {
      return {
        product: 'Socks',
-       image: './assets/socks_green.jpg'
+       image: './assets/socks_green.jpg',
+       inventory: 10
      }
    }
  })
