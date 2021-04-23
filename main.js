813d53d9de972fee1b9e67bf11cba221ff491f92
Jiří Kofránek <kofranek@gmail.com>
2021-04-24 01:32:17 +0200

    lesson  6

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ const app = Vue.createApp
 ({
    data () {
      return {
+       cart: 0,
        product: 'Socks',
        image: './assets/socks_green.jpg',
        inventory: 10,
        details: ['50% cotton', '30% wool', '20% polyester'],
        variants: [
-         { id: 2234, color: 'green' },
-         { id: 2235, color: 'blue' }
+         { id: 2234, color: 'green' , image: './assets/socks_green.jpg'},
+         { id: 2235, color: 'blue' , image: './assets/socks_blue.jpg'}
        ]
      }
+   },
+   methods: {
+     addToCart () {
+       this.cart += 1
+     },
+     // lesson 6 solution
+     // removeFromCart() {
+     //   if (this.cart >= 1) {
+     //     this.cart -= 1
+     //   }
+     // },
+     updateImage (variantImage) {
+        this.image=variantImage
+     }
+
    }
  })
