7e165b75549f625cf3d99698e19d9fb776cdfe0b
Jiří Kofránek <kofranek@gmail.com>
2021-04-24 00:48:33 +0200

    lesson  5

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp
      return {
        product: 'Socks',
        image: './assets/socks_green.jpg',
-       inventory: 10
+       inventory: 10,
+       details: ['50% cotton', '30% wool', '20% polyester'],
+       variants: [
+         { id: 2234, color: 'green' },
+         { id: 2235, color: 'blue' }
+       ]
      }
    }
  })
