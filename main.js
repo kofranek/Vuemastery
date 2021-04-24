24c717d93037c21f6772de3362b15ed6bfcd62b8
Jiří Kofránek <kofranek@gmail.com>
2021-04-24 02:19:25 +0200

    lesson  7

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp
        product: 'Socks',
        image: './assets/socks_green.jpg',
        inventory: 10,
+       inStock:true,
        details: ['50% cotton', '30% wool', '20% polyester'],
        variants: [
          { id: 2234, color: 'green' , image: './assets/socks_green.jpg'},
